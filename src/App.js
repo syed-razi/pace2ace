df9f5d032fa96c600eb5e36a7be3c05cb2fb2745
Razi <syedru@mcmaster.ca>
2021-09-20 18:30:00 -0400

    Change app component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import NavBar from "./Navbar";
 import Input from "./Input";
@@ -21,7 +21,7 @@ const TabPanel = (props) => {
       {value === index && children}
     </div>
   );
-}
+};
 
 const useStyles = (theme) => ({
   root: {
@@ -30,37 +30,34 @@ const useStyles = (theme) => ({
   },
 });
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      assignments: [],
-      assignmentID: 0,
-      monday: "",
-      tuesday: "",
-      wednesday: "",
-      thursday: "",
-      friday: "",
-      saturday: "",
-      sunday: "",
-      bufferTime: "",
-      minWorkTime: "",
-      value: 0,
-      saved: false,
-      edit: false,
-      editItem: {
-        className: "",
-        name: "",
-        worth: "",
-        startDate: new Date(),
-        dueDate: new Date(new Date().setHours(23, 59, 59)),
-        estimatedHours: "",
-        questions: [],
-      },
-    };
-  }
+const App = (props) => {
+  const [state, setState] = useState({
+    assignments: [],
+    assignmentID: 0,
+    monday: "",
+    tuesday: "",
+    wednesday: "",
+    thursday: "",
+    friday: "",
+    saturday: "",
+    sunday: "",
+    bufferTime: "",
+    minWorkTime: "",
+    value: 0,
+    saved: false,
+    edit: false,
+    editItem: {
+      className: "",
+      name: "",
+      worth: "",
+      startDate: new Date(),
+      dueDate: new Date(new Date().setHours(23, 59, 59)),
+      estimatedHours: "",
+      questions: [],
+    },
+  });
 
-  handleAddAssignment(
+  const handleAddAssignment = (
     className,
     name,
     worth,
@@ -69,11 +66,11 @@ class App extends React.Component {
     dueTime,
     estimatedHours,
     questions
-  ) {
-    this.setState((prevState) => ({
+  ) => {
+    setState((prevState) => ({
       assignmentID: prevState.assignmentID + 1,
       assignments: [
-        ...this.state.assignments,
+        ...state.assignments,
         {
           className: className,
           name: name,
@@ -87,21 +84,21 @@ class App extends React.Component {
         },
       ],
     }));
-  }
+  };
 
-  handleDeleteAssignment(aID) {
-    this.setState({
-      assignments: this.state.assignments.filter(({ id }) => id !== aID),
+  const handleDeleteAssignment = (aID) => {
+    setState({
+      assignments: state.assignments.filter(({ id }) => id !== aID),
     });
-  }
+  };
 
-  handleTabChange = (event, newValue) => {
-    this.setState({
+  const handleTabChange = (event, newValue) => {
+    setState({
       value: newValue,
     });
   };
 
-  handleSave = (
+  const handleSave = (
     monday,
     tuesday,
     wednesday,
@@ -112,7 +109,7 @@ class App extends React.Component {
     bufferTime,
     minWorkTime
   ) => {
-    this.setState({
+    setState({
       monday: monday,
       tuesday: tuesday,
       wednesday: wednesday,
@@ -126,9 +123,9 @@ class App extends React.Component {
     });
   };
 
-  handleEditAssignment(aID) {
-    console.log(this.state.editItem);
-    var editItem = this.state.assignments.find(
+  const handleEditAssignment = (aID) => {
+    console.log(state.editItem);
+    var editItem = state.assignments.find(
       ({
         className,
         name,
@@ -141,87 +138,85 @@ class App extends React.Component {
         id,
       }) => id === aID
     );
-    this.setState({
+    setState({
       edit: true,
       editItem: editItem,
     });
     console.log(editItem);
-    console.log(this.state.edit);
-    console.log(this.state.editItem);
-  }
+    console.log(state.edit);
+    console.log(state.editItem);
+  };
 
-  render() {
-    const { classes } = this.props;
+  const { classes } = props;
 
-    return (
-      <div className={classes.root}>
-        <NavBar />
-        <AppBar position="static">
-          <Tabs value={this.state.value} onChange={this.handleTabChange}>
-            <Tab label="Input" />
-            <Tab label="Preferences" />
-            <Tab label="Breakdown" disabled />
-          </Tabs>
-        </AppBar>
-        <TabPanel value={this.state.value} index={0}>
-          <Input
-            assignments={this.state.assignments}
-            onAddAssignment={this.handleAddAssignment.bind(this)}
-            onDeleteAssignment={this.handleDeleteAssignment.bind(this)}
-            onEditAssignment={this.handleEditAssignment.bind(this)}
-            editItem={this.state.editItem}
-          />
-        </TabPanel>
-        <TabPanel value={this.state.value} index={1}>
-          <Preferences onSave={this.handleSave} />
-        </TabPanel>
-        <TabPanel value={this.state.value} index={2}>
-          <Breakdown
-            assignments={
-              //   [{
-              //   className: "4HC3",
-              //   name: "Assignment 1",
-              //   worth: 3,
-              //   startDate: "12/03/20",
-              //   dueDate: "12/10/20",
-              //   dueTime: "11:59:00 PM",
-              //   estimatedHours: 10,
-              //   questions: [
-              //     {question: "Name", marks: 5},
-              //     {question: "Purpose", marks: 5},
-              //     {question: "Target User", marks: 5},
-              //     {question: "Tasks", marks: 25},
-              //     {question: "Sketches", marks: 30},
-              //     {question: "Layout", marks: 20},
-              //     {question: "Form", marks: 10},
-              //     ],
-              //   id: "1"
-              // }]
-              this.state.assignments
-            }
-            // monday={4}
-            // tuesday={3}
-            // wednesday={5}
-            // thursday={4}
-            // friday={3}
-            // saturday={3}
-            // sunday={0}
-            // bufferTime={2}
-            // minWorkTime={this.state.minWorkTime}
-            monday={this.state.monday}
-            tuesday={this.state.tuesday}
-            wednesday={this.state.wednesday}
-            thursday={this.state.thursday}
-            friday={this.state.friday}
-            saturday={this.state.saturday}
-            sunday={this.state.sunday}
-            bufferTime={this.state.bufferTime}
-            minWorkTime={this.state.minWorkTime}
-          />
-        </TabPanel>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.root}>
+      <NavBar />
+      <AppBar position="static">
+        <Tabs value={state.value} onChange={handleTabChange}>
+          <Tab label="Input" />
+          <Tab label="Preferences" />
+          <Tab label="Breakdown" disabled />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={state.value} index={0}>
+        <Input
+          assignments={state.assignments}
+          onAddAssignment={handleAddAssignment}
+          onDeleteAssignment={handleDeleteAssignment}
+          onEditAssignment={handleEditAssignment}
+          editItem={state.editItem}
+        />
+      </TabPanel>
+      <TabPanel value={state.value} index={1}>
+        <Preferences onSave={handleSave} />
+      </TabPanel>
+      <TabPanel value={state.value} index={2}>
+        <Breakdown
+          assignments={
+            //   [{
+            //   className: "4HC3",
+            //   name: "Assignment 1",
+            //   worth: 3,
+            //   startDate: "12/03/20",
+            //   dueDate: "12/10/20",
+            //   dueTime: "11:59:00 PM",
+            //   estimatedHours: 10,
+            //   questions: [
+            //     {question: "Name", marks: 5},
+            //     {question: "Purpose", marks: 5},
+            //     {question: "Target User", marks: 5},
+            //     {question: "Tasks", marks: 25},
+            //     {question: "Sketches", marks: 30},
+            //     {question: "Layout", marks: 20},
+            //     {question: "Form", marks: 10},
+            //     ],
+            //   id: "1"
+            // }]
+            state.assignments
+          }
+          // monday={4}
+          // tuesday={3}
+          // wednesday={5}
+          // thursday={4}
+          // friday={3}
+          // saturday={3}
+          // sunday={0}
+          // bufferTime={2}
+          // minWorkTime={state.minWorkTime}
+          monday={state.monday}
+          tuesday={state.tuesday}
+          wednesday={state.wednesday}
+          thursday={state.thursday}
+          friday={state.friday}
+          saturday={state.saturday}
+          sunday={state.sunday}
+          bufferTime={state.bufferTime}
+          minWorkTime={state.minWorkTime}
+        />
+      </TabPanel>
+    </div>
+  );
+};
 
 export default withStyles(useStyles)(App);
