71fa01ae52c1f8077176c1767b20f5137b5fa955
Razi <syedru@mcmaster.ca>
2021-09-23 13:30:01 -0400

    Change availability to array rather than object

--- src/Preferences/Preferences.js
+++ src/Preferences/Preferences.js
@@ -1,14 +1,8 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import { Typography } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = (theme) => ({
@@ -25,7 +19,6 @@ const useStyles = (theme) => ({
 });
 
 const Preferences = (props) => {
- 
   const { classes } = props;
 
   return (
@@ -39,134 +32,92 @@ const Preferences = (props) => {
         <Grid item>
           <form className={classes.form} noValidate autoComplete="off">
             <TextField
-              id="monday"
+              id="1"
               label="Monday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.monday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
+              value={props.availability[1]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
             <TextField
-              id="tuesday"
+              id="2"
               label="Tuesday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.tuesday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
-
+              value={props.availability[2]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
             <TextField
-              id="wednesday"
+              id="3"
               label="Wednesday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.wednesday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
+              value={props.availability[3]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
             <TextField
-              id="thursday"
+              id="4"
               label="Thursday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.thursday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
+              value={props.availability[4]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
             <TextField
-              id="friday"
+              id="5"
               label="Friday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.friday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
+              value={props.availability[5]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
             <TextField
-              id="saturday"
+              id="6"
               label="Saturday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.saturday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
+              value={props.availability[6]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
             <TextField
-              id="sunday"
+              id="0"
               label="Sunday"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-              value={props.availability.sunday}
-              onChange={(e) => props.onAvailabilityChange(e.target.id, e.target.value)}
+              value={props.availability[0]}
+              onChange={(e) =>
+                props.onAvailabilityChange(e.target.id, e.target.value)
+              }
             />
           </form>
         </Grid>
-        {/*<Button type="submit" variant="contained" color="primary" onClick={handleSave}>
-          Save
-        </Button>
-         <Grid item>
-          <Typography>
-            Please enter how much buffer time you would like for each
-            assignment:
-          </Typography>
-        </Grid>
-        <Grid item>
-          <form className={classes.form} noValidate autoComplete="off">
-            <TextField
-              id="bufferTime"
-              label="Buffer Time"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={this.state.bufferTime}
-              onChange={this.handleChange}
-            />
-          </form>
-        </Grid>
-        <Grid item>
-          <Typography>Please choose minimum work session time:</Typography>
-        </Grid>
-        <Grid item>
-          <form className={classes.form} noValidate autoComplete="off">
-            <TextField
-              id="minWorkTime"
-              label="Minimum Work Session Time"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={this.state.minWorkTime}
-              onChange={this.handleChange}
-            />
-          </form>
-        </Grid> */}
       </Grid>
-{/* 
-      <br />
-
-      <Dialog open={this.state.open} onClose={this.handleClose}>
-        <DialogContent>
-          <DialogContentText>
-            Your preferences have been saved
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleClose} color="primary" autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog> */}
     </Paper>
   );
 };
