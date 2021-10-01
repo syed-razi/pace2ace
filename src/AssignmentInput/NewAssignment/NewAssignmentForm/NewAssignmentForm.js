d2d2c7b78036a8787a8fef4aaa9f79ea05227288
Razi <syedru@mcmaster.ca>
2021-10-01 10:22:35 -0400

    Display questions if assignment has questions

--- src/AssignmentInput/NewAssignment/NewAssignmentForm/NewAssignmentForm.js
+++ src/AssignmentInput/NewAssignment/NewAssignmentForm/NewAssignmentForm.js
@@ -39,148 +39,142 @@ const NewAssignmentForm = (props) => {
         Enter Assignment Details Below
       </Typography>
       <Box sx={{ width: "100%" }}>
-        <form noValidate autoComplete="off">
-          <Stack alignItems="center" spacing={3} sx={{ width: "100%" }}>
-            <Stack
-              direction={{ xs: "column", md: "row" }}
-              spacing={3}
-              sx={{ width: "100%" }}
-            >
-              <TextField
-                fullWidth
-                id="standard-basic"
-                label="Class"
-                value={props.assignment.className}
-                onChange={(e) =>
-                  props.assignmentChangeHandlers.onChangeClass(e.target.value)
+        <Stack alignItems="center" spacing={3} sx={{ width: "100%" }}>
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={3}
+            sx={{ width: "100%" }}
+          >
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Class"
+              value={props.assignment.className}
+              onChange={(e) =>
+                props.assignmentChangeHandlers.onChangeClass(e.target.value)
+              }
+            />
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Assignment Name"
+              value={props.assignment.name}
+              onChange={(e) =>
+                props.assignmentChangeHandlers.onChangeName(e.target.value)
+              }
+            />
+          </Stack>
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={3}
+            sx={{ width: "100%" }}
+            justifyContent="center"
+          >
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Start Date"
+                inputFormat="MM/dd/yyyy"
+                value={props.assignment.startDate}
+                onChange={(newStartDate) =>
+                  props.assignmentChangeHandlers.onChangeStartDate(newStartDate)
                 }
+                renderInput={(params) => (
+                  <TextField sx={{ width: "100%" }} {...params} />
+                )}
               />
-              <TextField
-                fullWidth
-                id="standard-basic"
-                label="Assignment Name"
-                value={props.assignment.name}
-                onChange={(e) =>
-                  props.assignmentChangeHandlers.onChangeName(e.target.value)
+            </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Due Date"
+                inputFormat="MM/dd/yyyy"
+                value={props.assignment.dueDate}
+                onChange={(newDueDate) =>
+                  props.assignmentChangeHandlers.onChangeDueDate(newDueDate)
                 }
+                renderInput={(params) => (
+                  <TextField sx={{ width: "100%" }} {...params} />
+                )}
               />
-            </Stack>
-            <Stack
-              direction={{ xs: "column", md: "row" }}
-              spacing={3}
-              sx={{ width: "100%" }}
-              justifyContent="center"
-            >
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                  label="Start Date"
-                  inputFormat="MM/dd/yyyy"
-                  value={props.assignment.startDate}
-                  onChange={(newStartDate) =>
-                    props.assignmentChangeHandlers.onChangeStartDate(
-                      newStartDate
-                    )
-                  }
-                  renderInput={(params) => (
-                    <TextField sx={{ width: "100%" }} {...params} />
-                  )}
-                />
-              </LocalizationProvider>
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                  label="Due Date"
-                  inputFormat="MM/dd/yyyy"
-                  value={props.assignment.dueDate}
-                  onChange={(newDueDate) =>
-                    props.assignmentChangeHandlers.onChangeDueDate(newDueDate)
-                  }
-                  renderInput={(params) => (
-                    <TextField sx={{ width: "100%" }} {...params} />
-                  )}
-                />
-              </LocalizationProvider>
-            </Stack>
-            <Stack
-              direction={{ xs: "column", md: "row" }}
-              spacing={3}
-              sx={{ width: "100%" }}
-            >
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <TimePicker
-                  label="Due Time"
-                  value={props.assignment.dueDate}
-                  onChange={(newDueDate) =>
-                    props.assignmentChangeHandlers.onChangeDueDate(newDueDate)
-                  }
-                  renderInput={(params) => (
-                    <TextField sx={{ width: "100%" }} {...params} />
-                  )}
-                />
-              </LocalizationProvider>
-              <TextField
-                fullWidth
-                id="standard-number"
-                label="Estimated Hours"
-                type="number"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                value={props.assignment.estimatedHours}
-                onChange={(e) =>
-                  props.assignmentChangeHandlers.onChangeEstimatedHours(
-                    e.target.value
-                  )
+            </LocalizationProvider>
+          </Stack>
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={3}
+            sx={{ width: "100%" }}
+          >
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <TimePicker
+                label="Due Time"
+                value={props.assignment.dueDate}
+                onChange={(newDueDate) =>
+                  props.assignmentChangeHandlers.onChangeDueDate(newDueDate)
                 }
+                renderInput={(params) => (
+                  <TextField sx={{ width: "100%" }} {...params} />
+                )}
               />
-            </Stack>
-            {isAddingQuestions && (
-              <NewAssignmentQuestionsInput
-                assignment={props.assignment}
-                onAddQuestion={props.onAddQuestion}
-                onDeleteQuestion={props.onDeleteQuestion}
-              />
-            )}
-            {!isAddingQuestions && (
-              <Button
-                sx={{ width: { xs: "90%", md: "50%" } }}
-                variant="outlined"
-                startIcon={<AddIcon />}
-                onClick={() => setIsAddingQuestions(true)}
-              >
-                Add Questions
-              </Button>
-            )}
-            {props.isAddingAssignment && (
-              <Button
-                sx={{ width: { xs: "90%", md: "50%" } }}
-                variant="contained"
-                startIcon={<AddIcon />}
-                onClick={handleAddAssignment}
-                type="submit"
-              >
-                Add Assignment
-              </Button>
-            )}
-            {props.isEditingAssignment && (
-              <Button
-                sx={{ width: { xs: "90%", md: "50%" } }}
-                variant="contained"
-                startIcon={<SaveIcon />}
-                onClick={handleSaveAssignment}
-                type="submit"
-              >
-                Save
-              </Button>
-            )}
+            </LocalizationProvider>
+            <TextField
+              fullWidth
+              id="standard-number"
+              label="Estimated Hours"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              value={props.assignment.estimatedHours}
+              onChange={(e) =>
+                props.assignmentChangeHandlers.onChangeEstimatedHours(
+                  e.target.value
+                )
+              }
+            />
+          </Stack>
+          {(isAddingQuestions || props.assignment.questions.length > 0) && (
+            <NewAssignmentQuestionsInput
+              assignment={props.assignment}
+              onAddQuestion={props.onAddQuestion}
+              onDeleteQuestion={props.onDeleteQuestion}
+            />
+          )}
+          {!(isAddingQuestions || props.assignment.questions.length > 0) && (
+            <Button
+              sx={{ width: { xs: "90%", md: "50%" } }}
+              variant="outlined"
+              startIcon={<AddIcon />}
+              onClick={() => setIsAddingQuestions(true)}
+            >
+              Add Questions
+            </Button>
+          )}
+          {props.isAddingAssignment && (
             <Button
               sx={{ width: { xs: "90%", md: "50%" } }}
               variant="contained"
-              onClick={props.onClose}
+              startIcon={<AddIcon />}
+              onClick={handleAddAssignment}
             >
-              Close
+              Add Assignment
             </Button>
-          </Stack>
-        </form>
+          )}
+          {props.isEditingAssignment && (
+            <Button
+              sx={{ width: { xs: "90%", md: "50%" } }}
+              variant="contained"
+              startIcon={<SaveIcon />}
+              onClick={handleSaveAssignment}
+            >
+              Save
+            </Button>
+          )}
+          <Button
+            sx={{ width: { xs: "90%", md: "50%" } }}
+            variant="outlined"
+            onClick={props.onClose}
+          >
+            Close
+          </Button>
+        </Stack>
       </Box>
     </Stack>
   );
